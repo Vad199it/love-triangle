8a4cd3a1852b4f6b64af64dafd09f1f0e0b2e0d4
Минин А.В <MA@dc.transoil.gomel.by>
2019-02-23 16:52:59 +0300

    all tests

--- src/index.js
+++ src/index.js
@@ -3,5 +3,20 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+    var countLoversTriangle = 0;
+    for( var i = 0; i < preferences.length;i++)
+    {
+        preferences[i] = preferences[i]-1;
+    }
+    for( var i = 0; i < preferences.length; i++) {
+        if(preferences[i] < preferences.length && i !== preferences[i]) {
+            if (preferences[preferences[i]] < preferences.length){
+                if (preferences[preferences[preferences[i]]] === i){
+                    countLoversTriangle++;
+
+                }
+            }
+        }
+    }
+    return countLoversTriangle/3;
 };
